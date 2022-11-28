d02d427799a04446ad8b5aa2006cdcbd8e41ef67
saarf77 <saarf77@gmail.com>
2022-11-28 20:09:31 +0200

    initial

---
+++ src/cmps/app-footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="footer-container home-layout">
+    <section class="main-footer">
+      <div class="left-nav">
+        <p>© 2023 Triman, Inc.</p><p>·</p>
+        <p>About</p><p>·</p>
+        <p>Login</p><p>·</p>
+        <p>Become a host</p>
+      </div>
+        <div class="right-nav">
+        <img  src="../assets/imgs/icons/global.png" />
+        <p>English</p>
+        <div class="socials">
+          <ul class="main-nav clean-list flex">
+            <li class="social-links-footer flex">
+                <a href="https://www.instagram.com/airbnb/" target="_blank">
+                  <img src="../assets/img/icons/instagram.png" alt="">
+                </a>
+            </li>
+            <li class="social-links-footer flex">
+                <a href="https://www.facebook.com/airbnb/" target="_blank">
+                  <img src="../assets/img/icons/facebook.png" alt="">
+                </a>
+            </li>
+            <li class="social-links-footer flex">
+                <a href="https://www.linkedin.com/company/airbnb/" target="_blank">
+                    <img src="../assets/img/icons/linkedin.png" alt="">
+                </a>
+            </li>
+        </ul>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
---
+++ src/root-cmp.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <user-msg/>
+    <app-header />
+    <router-view/>
+    <app-footer/>
+  </section>
+</template>
+
+<script>
+
+
+import { store } from './store/store'
+import appHeader from './cmps/app-header.vue'
+import appFooter from './cmps/app-footer.vue'
+import userMsg from './cmps/user-msg.vue'
+import { userService } from './services/user.service'
+
+
+export default {
+
+  created() {
+    console.log('Vue App created')
+    const user = userService.getLoggedinUser()
+    if (user)  store.commit({type: 'setLoggedinUser', user})
+  },
+  components: {
+    appHeader,
+    userMsg,
+    appFooter,
+  },
+}
+</script>
