101436bd8e124df20753d6c25cefc630617dfcc7
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-29 20:31:02 +0200

    fix car.service to stays.service

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -24,7 +24,24 @@ export default {
 </script>
 
 <template>
-    <section class="notifications-page home-layout">
-          <div>stay details</div>
+    <section class="details-page">
+        <section class="short-display">
+          <div class="name"></div>
+          <div class="star-score"></div>
+          <div class="reviews-count"></div>
+          <div class="label-list"></div>
+          <button class="details-btn share"></button>
+          <button class="details-btn save"></button>
+        </section>
+        <section class="photo-display">
+            <img class="main-photo" src="" alt="">
+            <img class="photo-item" src="" alt="">
+            <img class="photo-item" src="" alt="">
+            <img class="photo-item" src="" alt="">
+            <img class="photo-item" src="" alt="">
+        </section>
+    </section>
+    <section class="details-page model">
+          
     </section>
 </template>
