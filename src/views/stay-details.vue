0e008927e8d4636fd0a14b9025641037e4caf3f0
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-29 17:59:08 +0200

    update router.js with stay-details route

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -1,16 +1,11 @@
-<template>
-    <section class="notifications-page home-layout">
-          <div>stay details</div>
-    </section>
-
-</template>
-
 <script>
 export default {
 
 }
 </script>
 
-<style>
-
-</style>
+<template>
+    <section class="notifications-page home-layout">
+          <div>stay details</div>
+    </section>
+</template>
