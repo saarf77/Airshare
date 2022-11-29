5e56f7ba89daa1348742092ac15fa64a5b20d67e
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-29 18:32:18 +0200

    created: An initial scheme for handling the details pages

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -1,4 +1,23 @@
+
+
+
 <script>
+// page structure => 
+// stay name
+// stay labels  + share || save
+// stay photos  {{photos details CMP}}
+// stay description + host img || {{order CMP}}
+// stay achievement 
+// stay cover ad 
+// stay "host depiction"
+// stay options as icons
+// order dates picker
+// stay reviews
+// map section
+// more details 
+
+// {{ stay-details model page for: reviews, description, extended options as icons }}
+
 export default {
 
 }
