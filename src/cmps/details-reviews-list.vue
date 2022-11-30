3f154cd74667d6cde6f865fd0b7ef42deb0567d1
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-30 12:41:29 +0200

    creating sub cmps for the details page and svg.service

---
+++ src/cmps/details-reviews-list.vue
@@ -0,0 +1,66 @@
+<script>
+import {svgService} from '../services/svg.service.js';
+export default {
+    data(){
+        return {
+
+        }
+    }, 
+    computed: {
+    svgBlackStar(){
+            return svgService.getSvgIcon('blackStarIcon');
+        }
+    },
+    components:{
+        svgService,
+    }
+}
+</script>
+
+<template>
+       <section class="reviews-display">
+            <div class="details-title">
+                {{svgBlackStar}}
+                <span class="average-reviews">5</span>
+                ·
+                <span class="reviewers-count">5</span>
+                reviews
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Cleanliness</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Communication</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Check-in</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Accuracy</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Location</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+            <div class="stay-attributes">
+                <div class="attribute">
+                    <div class="attribute-name">Value</div>
+                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
+                </div>
+            </div>
+        </section>
+</template>
---
+++ src/services/svg.service.js
@@ -0,0 +1,12 @@
+ const svgObj = {
+       airConditioner: `<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;"><path d="M17 1v4.03l4.026-2.324 1 1.732L17 7.339v6.928l6-3.464V5h2v4.648l3.49-2.014 1 1.732L26 11.381l4.026 2.325-1 1.732L24 12.535l-6 3.464 6 3.465 5.026-2.902 1 1.732L26 20.618l3.49 2.016-1 1.732L25 22.351V27h-2v-5.804l-6-3.465v6.929l5.026 2.902-1 1.732L17 26.97V31h-2v-4.031l-4.026 2.325-1-1.732L15 24.66v-6.929l-6 3.464V27H7v-4.65l-3.49 2.016-1-1.732 3.489-2.016-4.025-2.324 1-1.732 5.025 2.901 6-3.464-6-3.464-5.025 2.903-1-1.732L6 11.38 2.51 9.366l1-1.732L7 9.648V5h2v5.803l6 3.464V7.339L9.974 4.438l1-1.732L15 5.03V1z"></path></svg>`,
+       blackStarIcon: `<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 16px; width: 16px; fill: currentcolor;"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" fill-rule="evenodd"></path></svg>`,
+}
+
+export const svgService = { 
+       getSvgIcon, 
+}
+
+function getSvgIcon(svgName) {
+       return svgObj[svgName];
+}
