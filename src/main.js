7267c3ec25b55e0e0864b32977b2b14b140e7fe5
saarf77 <saarf77@gmail.com>
2022-11-30 14:56:40 +0200

    fonts

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import VCalendar from 'v-calendar';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/styles/main.scss'
+import './assets/styles/setup/_typography.scss'
 import rootCmp from './root-cmp.vue'
 
 const app = createApp(rootCmp)
