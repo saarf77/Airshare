d61d8ff0663ba48de9ddb60ea45eb2005e0f4f84
saarf77 <saarf77@gmail.com>
2022-11-30 16:38:57 +0200

    h

--- src/cmps/explore-filter.vue
+++ src/cmps/explore-filter.vue
@@ -19,8 +19,7 @@
           :class="{ 'active-btn': isExpend ? isActive : !isActive }">
           <label for="where">Where</label>
 
-          <input id="where" list="where" name="where" v-model="filterBy.where" placeholder="Search destination" />
-
+          <input id="where" list="where" name="where" v-model="filterBy.where" placeholder="Search destinations" />
         </div>
         <div class="filter-option check">
           <div class="labels-wrap">
@@ -33,25 +32,16 @@
                     <input name="checkin" :value="inputValue.start" v-on="inputEvents.start" placeholder="Add dates"
                       class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300" />
                   </div>
-                  <!-- add choose dates and im flexible to the date picker -->
                   <div :class="{ 'active-btn': endActive }" class="checkout" @click.native="activeTab('checkout')">
                     <label for="checkout">Check out</label>
                     <input name="checkout" :value="inputValue.end" v-on="inputEvents.end" placeholder="Add dates"
                       class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300" />
                   </div>
-                  <!-- todo: -->
-                  <span>exact dates</span>
-                  <span>1 day</span>
-                  <span>2 days</span>
-                  <span>3 days</span>
-                  <span>7 days</span>
-
                 </div>
               </template>
             </v-date-picker>
           </div>
         </div>
-        
         <div @click.native="activeTab('guest')" class="filter-option guest-dropdown"
           :class="{ 'active-btn': guestActive }">
           <div @click="dropDownMenu($event)" class="add-guest-wrapper">
@@ -64,8 +54,7 @@
               <div v-for="i in 100" class="cell"></div>
               <div class="content filter-content">
                 <button class="action-btn">
-                  <!-- <div class="material-icons search"> search </div> -->
-                  <span>Search</span>
+                  <div class="material-icons search"> search </div>
                 </button>
               </div>
             </div>
@@ -124,7 +113,7 @@
     <div class="row-card flex">
       <div class="lft-crd">
         <span class="title-sm"> Pets</span>
-        <span class="txt-sm service-animal">Bringing a service animal?</span>
+        <span class="txt-sm">Serve animals</span>
       </div>
       <div class="rit-crd guests-btns">
         <button @click.stop="updateGuests('pets', -1)">
@@ -202,7 +191,45 @@ export default {
         this.endActive = true;
       }
     },
- 
+    formSubmit() {
+      this.isShow = !this.isShow;
+      eventBus.emit('overlay', this.isShow);
+      eventBus.emit('getFilterStay');
+      let url = `/explore?location=${this.filterBy.where}`;
+      this.$router.push(url);
+
+      this.$emit('expendForm', false);
+    },
+    expendForm(value) {
+      this.firstClick = value;
+      this.$emit('expendForm', true);
+    },
+    showInitModal(ev) {
+      this.toggleShowModal();
+
+    },
+    dropDownMenu() {
+      this.showModal = true;
+    },
+    updateGuests(type, number) {
+      this.filterBy.guests[type] += number;
+    },
+    toggleShowModal(ev) {
+      this.showModal = !this.showModal;
+    },
+    onClickAway(ev) {
+      this.showModal = false;
+      if (this.firstClick) {
+        this.isActive = true;
+        this.firstClick = false;
+      } else {
+        this.isActive = false;
+      }
+    },
+
+    setFilter(loc) {
+      this.filterBy.where = loc
+    }
   },
 
   created() {
