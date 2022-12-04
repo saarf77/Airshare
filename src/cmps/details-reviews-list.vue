f20f82804fcfe4112873f039cf3106c55bc6c51d
ohad bergmann <ohad.bergmann@gmail.com>
2022-12-04 12:16:33 +0200

    post-meeting - reviews attributes and feedback part - 2

--- src/cmps/details-reviews-list.vue
+++ src/cmps/details-reviews-list.vue
@@ -1,8 +1,10 @@
 <script>
 import {svgService} from '../services/svg.service.js';
 export default {
+    props: ['reviewsList'],
     data(){
         return {
+            currStayReviews: [],
             currStayStarRating: 0,
             currStayAttributes: {
                 cleanlinessRating: 0,
@@ -18,6 +20,47 @@ export default {
     svgBlackStar(){
             const svgHtml = svgService.getSvgIcon('blackStarIcon') + `<span class="average-reviews">5</span>·<span class="reviewers-count">5</span>reviews`
             return svgHtml;
+        },
+        stayAttributesHtml(){
+            let strHtml = '' 
+            if(this.currStayReviews?.length > 0 ){
+                let attrStr = '';
+                let idx = -1;
+                strHtml = '<section class="attr-group">'
+                for (const key in this.currStayAttributes) {
+                    attrStr = key +'';
+                    idx = attrStr.indexOf('Rating');
+                    attrStr = attrStr.substring(0, 1).toLocaleUpperCase() + attrStr.substring(1,idx);
+                    if(attrStr === 'CheckIn'){attrStr = 'Check-in';}
+                   strHtml += `<div class="attribute"><label class="attribute-name">${attrStr}</label><div class="attr-bar"><span class="attr-score-bar"></span>
+                    <span class="attr-score-value" style="width: calc(120px * ${this.currStayAttributes[key]}/5)"></span>
+                    <span class="attr-score-num">${this.currStayAttributes[key]}</span></div></div>`;
+                    if(attrStr === 'Check-in'){strHtml += '</section><section class="attr-group">';}
+                }
+                strHtml += '</section>';
+               
+            }
+            return strHtml;
+        },
+    }, 
+    watch:{
+        reviewsList:{
+            handler(newVal) {
+                if(newVal?.list.length > 0){
+                    this.currStayStarRating = (newVal.list.reduce((acc, review) => acc + parseFloat(review.starRate), 0)/newVal.list.length).toFixed(1) ;
+                    this.currStayAttributes.cleanlinessRating = (newVal.list.reduce((acc, review) => acc + review.attributes.cleanliness, 0)/newVal.list.length).toFixed(1);
+                    this.currStayAttributes.communicationRating = (newVal.list.reduce((acc, review) => acc + review.attributes.communication, 0)/newVal.list.length).toFixed(1);
+                    this.currStayAttributes.checkInRating = (newVal.list.reduce((acc, review) => acc += review.attributes.checkIn, 0)/newVal.list.length).toFixed(1);
+                    this.currStayAttributes.accuracyRating = (newVal.list.reduce((acc, review) => acc += review.attributes.accuracy, 0)/newVal.list.length).toFixed(1);
+                    this.currStayAttributes.locationRating = (newVal.list.reduce((acc, review) => acc += review.attributes.location, 0)/newVal.list.length).toFixed(1);
+                    this.currStayAttributes.valueRating = (newVal.list.reduce((acc, review) => acc += review.attributes.value, 0)/newVal.list.length).toFixed(1);
+
+                    this.currStayReviews = newVal.list;
+                }
+
+               
+            },
+            deep: true
         }
     },
     components:{
@@ -30,41 +73,8 @@ export default {
        <section class="reviews-display">
             <div class="details-title" v-html="svgBlackStar">
             </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Cleanliness</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
-            </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Communication</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
-            </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Check-in</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
-            </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Accuracy</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
-            </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Location</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
-            </div>
-            <div class="stay-attributes">
-                <div class="attribute">
-                    <div class="attribute-name">Value</div>
-                    <div class="attribute-bar"><span class="attribute-score-bar"></span></div>
-                </div>
+           
+            <div class="stay-attributes" v-html="stayAttributesHtml">
             </div>
         </section>
 </template>
