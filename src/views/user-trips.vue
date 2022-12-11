5b6108c0e72da2ec84395e70f5d92ea1c0166f7f
Lior Keizman <liork33@gmail.com>
2022-12-11 22:12:21 +0200

    l

--- src/views/user-trips.vue
+++ src/views/user-trips.vue
@@ -1,24 +1,43 @@
 <template>
-    <table class="content-table">
-        <thead>
+    <table class="content-table main-layout">
+        <!-- <thead>
             <tr>
-                <th class="title-thead"></th>
-                <th class="title-thead">Stay name</th>
-                <th class="title-thead">Total price payed</th>
-                <th class="title-thead">Address</th>
-                <th class="title-thead">Actions</th>
+                <th class="title-thead text-center"></th>
+                <th class="title-thead text-center">Stay name</th>
+                <th class="title-thead text-center">Total price payed</th>
+                <th class="title-thead text-center">Address</th>
+                <th class="title-thead text-center">Actions</th>
             </tr>
-        </thead>
+        </thead> -->
+
+        <el-table :data="tableData" style="width: 100%"  class="border-bottom">
+    <!-- <el-table-column fixed prop="Guest" label="Guest" width="140" /> -->
+    <el-table-column prop="Stay name" label="Stay name" width="735" />
+    <el-table-column prop="Price" label="Price" width="335" />
+    <el-table-column prop="Status" label="Status" width="355" />
+    <!-- <el-table-column prop="Guests" label="Guests" width="120" /> -->
+    <!-- <el-table-column prop="Booking" label="Booking" width="120" /> -->
+    <!-- <el-table-column prop="Action" label="Action" width="120" /> -->
+    <el-table-column prop="Check In/ Out" label="Check In/ Out" width="190" />
+    <!-- <el-table-column fixed="right" label="Operations" width="130">
+      <template #default>
+        <el-button link type="primary" size="small" @click=""
+          >Detail</el-button>
+        <el-button link type="primary" size="small">Edit</el-button>
+      </template>
+    </el-table-column> -->
+  </el-table>
+
 
         <tbody>
             <div v-for="trip in getOrders" :key="trip._id">
-                <tr class="row-table border_bottom">
+                <tr class="row-table border-bottom">
                     <!-- <td><img class="stay-image" :src=imageUrl></td> -->
                     <td>{{ getStayName(trip) }}</td>
                     <td>{{ trip.totalPrice }}</td>
                     <td>{{ trip.status }}</td> 
-                    <td>{{ getStartDate(trip)}}</td>
-                    <td>{{getEndDate(trip)}}</td>
+                    <td>{{ getStartDate(trip)}} - {{getEndDate(trip)}}</td>
+                    <!-- <td>{{getEndDate(trip)}}</td> -->
                     <!-- <pre>{{trip}}</pre> -->
                 </tr>
             </div>
